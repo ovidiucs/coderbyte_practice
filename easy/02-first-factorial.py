26807f551068e1074e2a096415003260d2ab04f3
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-04 11:00:41 +0300

    rename, add

---
+++ easy/02-first-factorial.py
@@ -0,0 +1,20 @@
+"""
+Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it 
+(ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, 
+the range will be between 1 and 18. 
+
+Use the Parameter Testing feature 
+in the box below to test your code with different arguments.
+"""
+
+from math import factorial
+
+def FirstFactorial(num): 
+
+  return factorial(num)
+    
+    
+# keep this function call here  
+# to see how to enter arguments in Python scroll down
+print FirstFactorial(raw_input())           
+
