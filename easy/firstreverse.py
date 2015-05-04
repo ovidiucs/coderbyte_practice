c6e804e180134f250d0f30f305afc8b6288baaf5
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-04 10:56:38 +0300

    First Commit

---
+++ easy/firstreverse.py
@@ -0,0 +1,8 @@
+#Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+#Use the Parameter Testing feature in the box below to test your code with different arguments.
+def FirstReverse(str):
+  return str[::-1]
+
+  print FirstReverse(raw_input())           
+
