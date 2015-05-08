6b152ca1810e32af8653ee8bda29d9f79d0dc8eb
ovidiucs <ovidiu.ctin@gmail.com>
2015-05-08 11:20:36 +0300

    integer base 2

---
+++ medium/09-BinaryConverter.py
@@ -0,0 +1,13 @@
+__author__ = 'ovidiucs'
+def BinaryConverter(str):
+
+    return int(str,2)
+
+
+
+# keep this function call here
+# to see how to enter arguments in Python scroll down
+print BinaryConverter("101")
+# Using the Python language, have the function BinaryConverter(str) return the
+# decimal form of the binary value. For example: if 101 is passed return 5, or if
+# 1000 is passed return 8.
